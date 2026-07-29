23816a465b2c2d108a8a9d8a0941e35dd438a7ec
UniInspect <85514596+insurance-inspect@users.noreply.github.com>
2026-07-29 12:22:41 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // UniInspect Service Worker
 // Caches app for offline use
 
-const CACHE_NAME = "uninspect-v22";
+const CACHE_NAME = "uninspect-v23";
 const URLS_TO_CACHE = [
   "/UniInspect/",
   "/UniInspect/index.html",
